bff831edd9de02fcc623084981cd41fd242b0367
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-10-31 12:58:24 +0100

    Update the tests

--- test.ps1
+++ test.ps1
@@ -18,47 +18,49 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 
-$VERSION = "1.0.117.0"
-$SHA256 = "D35CB72316BF55349305FB0698C52C8B8117127A3211FF163C288FA2A7F9B633"
-$ZIPNAME = "sqlite-netStandard20-binary-$VERSION.zip"
-$TOOLS = "sqlite-tools-win32-x86-3400000"
+$VERSION = '1.0.118.0'
+$SHA256 = '21093e5ffa803009c6b02e5f5495b5e07971fd0371c667359960419068a432f2'
+$ZIPNAME = ('sqlite-netStandard20-binary-{0}.zip' -f $VERSION)
+$TOOLS = 'sqlite-tools-win32-x86-3430200.zip'
 
-$ErrorActionPreference = "Stop"
-$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
 
 trap
 {
-	throw $PSItem
+   throw $PSItem
 }
 
-foreach ($Name in "bin", "obj") {
-	if (Test-Path "$Name") {
-		Remove-Item "$Name" -Force -Recurse
-	} 
-}
-
-& "dotnet.exe" build "test.csproj" --configuration Release
-
-If ( $LastExitCode -ne 0 )
+foreach ($Name in 'bin', 'obj')
 {
-	Exit $LastExitCode
+   if (Test-Path -Path $Name)
+   {
+      Remove-Item -Path $Name -Force -Recurse
+   }
 }
 
-If (-not(Test-Path "test.db"))
+& "$env:ProgramW6432\dotnet\dotnet.exe" build 'test.csproj' --configuration Release
+
+if ($LastExitCode -ne 0)
 {
-	Write-Host "Following should succeed and create a database"
+   exit $LastExitCode
+}
 
-	if (-not(Test-Path -Path "$TOOLS"))
-	{
-		if (-not(Test-Path -Path "$TOOLS.zip"))
-		{
-			Invoke-WebRequest -Uri "https://www.sqlite.org/2022/$TOOLS.zip" -OutFile "$TOOLS.zip"
-		}
-
-		Expand-Archive -Path "$TOOLS.zip" -DestinationPath .
-	}
-
-@"
+if (-not (Test-Path -Path 'test.db'))
+{
+   Write-Output -InputObject 'Following should succeed and create a database'
+   
+   if (-not (Test-Path -Path $TOOLS))
+   {
+      if (-not (Test-Path -Path ('{0}.zip' -f $TOOLS)))
+      {
+         Invoke-WebRequest -Uri ('https://www.sqlite.org/2023/{0}.zip' -f $TOOLS) -OutFile ('{0}.zip' -f $TOOLS)
+      }
+      
+      Expand-Archive -Path ('{0}.zip' -f $TOOLS) -DestinationPath .
+   }
+   
+   @"
 CREATE TABLE MESSAGES (
 	CONTENT VARCHAR(256)
 );
@@ -67,91 +69,106 @@ INSERT INTO MESSAGES (CONTENT) VALUES ('Hello World');
 
 SELECT * FROM MESSAGES;
 "@ | & "$TOOLS\sqlite3.exe" test.db
-
-	If ( $LastExitCode -ne 0 )
-	{
-		Exit $LastExitCode
-	}
+   
+   if ($LastExitCode -ne 0)
+   {
+      exit $LastExitCode
+   }
 }
 
-if (("$env:PROCESSOR_ARCHITECTURE" -eq "x86") -or ("$env:PROCESSOR_ARCHITECTURE" -eq "AMD64"))
+if (("$env:PROCESSOR_ARCHITECTURE" -eq 'x86') -or ("$env:PROCESSOR_ARCHITECTURE" -eq 'AMD64'))
 {
-	Write-Host "Following should succeed and read the database"
-
-	& "dotnet.exe" "bin\Release\net6.0\test.dll"
-
-	If ( $LastExitCode -ne 0 )
-	{
-		Exit $LastExitCode
-	}
+   Write-Output -InputObject 'Following should succeed and read the database'
+   
+   & "$env:ProgramW6432\dotnet\dotnet.exe" 'bin\Release\net6.0\test.dll'
+   
+   if ($LastExitCode -ne 0)
+   {
+      exit $LastExitCode
+   }
 }
 
-Remove-Item "bin\Release\net6.0\runtimes" -Force -Recurse
+Remove-Item -Path 'bin\Release\net6.0\runtimes' -Force -Recurse
 
-Write-Host "Following should fail with missing SQLite.Interop.dll"
+Write-Output -InputObject 'Following should fail with missing SQLite.Interop.dll'
 
-& "dotnet.exe" "bin\Release\net6.0\test.dll"
+& "$env:ProgramW6432\dotnet\dotnet.exe" 'bin\Release\net6.0\test.dll'
 
-If ( $LastExitCode -eq 0 )
+if ($LastExitCode -eq 0)
 {
-	throw "This should have failed with no SQLite.Interop.dll"
+   throw 'This should have failed with no SQLite.Interop.dll'
 }
 
-if (-not(Test-Path "runtimes"))
+if (-not (Test-Path -Path 'runtimes'))
 {
-	Expand-Archive -Path SQLite.Interop-$VERSION-win.zip -DestinationPath "."
+   Expand-Archive -Path SQLite.Interop-$VERSION -win .zip -DestinationPath '.'
 }
 
-switch ( "$env:PROCESSOR_ARCHITECTURE" )
+switch ("$env:PROCESSOR_ARCHITECTURE")
 {
-	"x86"   { Copy-Item -LiteralPath "runtimes\win-x86\native\SQLite.Interop.dll"   -Destination "bin\Release\net6.0" }
-	"AMD64" { Copy-Item -LiteralPath "runtimes\win-x64\native\SQLite.Interop.dll"   -Destination "bin\Release\net6.0" }
-	"ARM"   { Copy-Item -LiteralPath "runtimes\win-arm\native\SQLite.Interop.dll"   -Destination "bin\Release\net6.0" }
-	"ARM64" { Copy-Item -LiteralPath "runtimes\win-arm64\native\SQLite.Interop.dll" -Destination "bin\Release\net6.0" }
-	default { throw "Unknown architecure" }
+   'x86'
+   {
+      Copy-Item -LiteralPath 'runtimes\win-x86\native\SQLite.Interop.dll' -Destination 'bin\Release\net6.0'
+   }
+   'AMD64'
+   {
+      Copy-Item -LiteralPath 'runtimes\win-x64\native\SQLite.Interop.dll' -Destination 'bin\Release\net6.0'
+   }
+   'ARM'
+   {
+      Copy-Item -LiteralPath 'runtimes\win-arm\native\SQLite.Interop.dll' -Destination 'bin\Release\net6.0'
+   }
+   'ARM64'
+   {
+      Copy-Item -LiteralPath 'runtimes\win-arm64\native\SQLite.Interop.dll' -Destination 'bin\Release\net6.0'
+   }
+   default
+   {
+      throw 'Unknown architecure'
+   }
 }
 
-Write-Host "Following should fail with missing entry point SI7fca2652f71267db in SQLite.Interop.dll"
+Write-Output -InputObject 'Following should fail with missing entry point SI7fca2652f71267db in SQLite.Interop.dll'
 
-& "dotnet.exe" "bin\Release\net6.0\test.dll"
+& "$env:ProgramW6432\dotnet\dotnet.exe" 'bin\Release\net6.0\test.dll'
 
-If ( $LastExitCode -eq 0 )
+if ($LastExitCode -eq 0)
 {
-	throw "This should have failed with missing entry point SI7fca2652f71267db in SQLite.Interop.dll"
+   throw 'This should have failed with missing entry point SI7fca2652f71267db in SQLite.Interop.dll'
 }
 
-if (-not(Test-Path -Path $ZIPNAME))
+if (-not (Test-Path -Path $ZIPNAME))
 {
-	Invoke-WebRequest -Uri "https://system.data.sqlite.org/blobs/$VERSION/$ZIPNAME" -OutFile $ZIPNAME
+   Invoke-WebRequest -Uri ('https://system.data.sqlite.org/blobs/{0}/{1}' -f $VERSION, $ZIPNAME) -OutFile $ZIPNAME
 }
 
-Remove-Item "bin\Release\net6.0\System.Data.SQLite.dll"
+Remove-Item -Path 'bin\Release\net6.0\System.Data.SQLite.dll'
 
-$null = New-Item -Path "." -Name "tmp" -ItemType "directory"
+$null = New-Item -Path '.' -Name 'tmp' -ItemType 'directory'
 
 try
 {
-	Expand-Archive -LiteralPath "$ZIPNAME" -DestinationPath "tmp"
-
-	if ((Get-FileHash -LiteralPath $ZIPNAME -Algorithm "SHA256").Hash -ne $SHA256)
-	{
-		throw "SHA256 mismatch for $ZIPNAME"
-	}
-
-	$null = Move-Item -Path "tmp\System.Data.SQLite.dll" -Destination "bin\Release\net6.0"
+   Expand-Archive -LiteralPath $ZIPNAME -DestinationPath 'tmp'
+   
+   if ((Get-FileHash -LiteralPath $ZIPNAME -Algorithm 'SHA256').Hash -ne $SHA256)
+   {
+      throw ('SHA256 mismatch for {0}' -f $ZIPNAME)
+   }
+   
+   $null = Move-Item -Path 'tmp\System.Data.SQLite.dll' -Destination 'bin\Release\net6.0'
 }
 finally
 {
-	Remove-Item "tmp" -Force -Recurse
+   Remove-Item -Path 'tmp' -Force -Recurse
 }
 
-Write-Host "Following should succeed and read the database"
+Write-Output -InputObject 'Following should succeed and read the database'
 
-& "dotnet.exe" "bin\Release\net6.0\test.dll"
+& "$env:ProgramW6432\dotnet\dotnet.exe" 'bin\Release\net6.0\test.dll'
 
-If ( $LastExitCode -ne 0 )
+if ($LastExitCode -ne 0)
 {
-	Exit $LastExitCode
+   exit $LastExitCode
 }
 
-Write-Host "Tests complete"
+Write-Output -InputObject 'Tests complete'
